4b51b22cf02c2342e70beea37c847c2c4d7a2489
baal <prasimbaal@gmail.com>
2022-03-11 10:22:00 -0800

    size prediction script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,12 @@
 int main(void)
 {
     char long_int = "long int";
+    char statement = "char";
+    char intiger = "int";
     char long_long = "long long int";
     char float = "float";
-    printf("Size of a char: 1 byte(s)\n");
-    printf("Size of an int: 4 byte(s)\n);
+    printf("Size of a %s: 1 byte(s)\n", statement);
+    printf("Size of an %s: 4 byte(s)\n", intiger);
     printf("Size of a %s: 4 byte(s)\n",long_int);
     printf("Size of a %s: 8 byte(s)\n", long_long);
     printf("Size of a %s: 4 byte(s)\n",float);
