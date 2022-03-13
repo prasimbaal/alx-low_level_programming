5e6dcaeb4a6c97d0c9292384d17ab99df85a678c
baal <prasimbaal@gmail.com>
2022-03-12 23:01:50 -0800

    size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@ int main(void) /*outputs the size of variable in c*/
 {
     printf("Size of a %s: 1 byte(s)\n", "char");
     printf("Size of an %s: 4 byte(s)\n", "int");
-    printf("Size of a %s: 4 byte(s)\n","long int");
+    printf("Size of a %s: 4 byte(s)\n", "long int");
     printf("Size of a %s: 8 byte(s)\n", "long long int");
     printf("Size of a %s: 4 byte(s)\n", "float");
     return (0);
