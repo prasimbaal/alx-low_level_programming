395e4ba0971b479ade06fcb1ce41d81a0a896482
baal <prasimbaal@gmail.com>
2022-03-12 22:48:38 -0800

    Sizes of variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,10 @@
 
 int main(void)
 {
-    char long_int = "long int";
-    char statement = "char";
-    char intiger = "int";
-    char long_long = "long long int";
-    char float = "float";
-    printf("Size of a %s: 1 byte(s)\n", statement);
-    printf("Size of an %s: 4 byte(s)\n", intiger);
-    printf("Size of a %s: 4 byte(s)\n",long_int);
-    printf("Size of a %s: 8 byte(s)\n", long_long);
-    printf("Size of a %s: 4 byte(s)\n",float);
+    printf("Size of a %s: 1 byte(s)\n", "statement");
+    printf("Size of an %s: 4 byte(s)\n", "intiger");
+    printf("Size of a %s: 4 byte(s)\n","long_int");
+    printf("Size of a %s: 8 byte(s)\n", "long_long");
+    printf("Size of a %s: 4 byte(s)\n","float");
+    return (0)_;
 }
