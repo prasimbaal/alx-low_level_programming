0afdf4dba4f878172ca0916bdb26bb1d658dd8fc
baal <prasimbaal@gmail.com>
2022-03-12 22:58:26 -0800

    C variables size print code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) /*outputs the size of variable in c*/
 {
-    printf("Size of a %s: 1 byte(s)\n", "statement");
-    printf("Size of an %s: 4 byte(s)\n", "intiger");
-    printf("Size of a %s: 4 byte(s)\n","long_int");
-    printf("Size of a %s: 8 byte(s)\n", "long_long");
-    printf("Size of a %s: 4 byte(s)\n","float");
+    printf("Size of a %s: 1 byte(s)\n", "char");
+    printf("Size of an %s: 4 byte(s)\n", "int");
+    printf("Size of a %s: 4 byte(s)\n","long int");
+    printf("Size of a %s: 8 byte(s)\n", "long long int");
+    printf("Size of a %s: 4 byte(s)\n", "float");
     return (0);
 }
