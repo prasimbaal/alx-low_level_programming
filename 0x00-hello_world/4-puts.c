5870a7697efd8338aa67f83bc6fdd5a1df4a9aa8
Bealprasim <prasimbaal@gmail.com>
2022-06-02 10:50:08 -0700

    4-puts file improved to betty linter standards

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* prints string*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
