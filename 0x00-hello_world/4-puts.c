315e73a78be97b0a86a384cae8bf97f282709196
Bealprasim <prasimbaal@gmail.com>
2022-06-02 10:56:52 -0700

    Function comment updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-
-int main(void) /* prints string*/
+/**
+ * prints string
+ */
+int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
 	return (0);
