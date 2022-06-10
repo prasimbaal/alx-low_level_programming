bda95380b199633aa1b07d50d68d720a6f21d1a0
Bealprasim <prasimbaal@gmail.com>
2022-06-10 14:50:46 -0700

    prints a to z

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/* prints the last digit of a random number*/
+
+int main(void)
+{
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  /* your code goes there */
+  if 
+  return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  char alphabet;
+
+  for (alphabet = 'a'; alphabet <= 'z'; alphabet++){
+    putchar(alphabet);
+    putchar('\n');
+
+  }
+return (0);
+}
