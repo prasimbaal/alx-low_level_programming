55797d96a16841f39d5587a5919b2fe09a512150
prasimbaal <prasimbaal@gmail.com>
2022-09-11 02:25:24 -0700

    Prints numbers from 1 to 9

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,10 +2,10 @@
 
 int main(void) /*prints single digit base 10 numbers*/
 {
-int i; /*variable for the natural numbers*/
+int i;
 for (i = 0; i < 10; i++) /*excludes 10 from the loop*/
 {
-putchar(i);
+printf("%d",i);
 }
 putchar('\n');
 return (0);
