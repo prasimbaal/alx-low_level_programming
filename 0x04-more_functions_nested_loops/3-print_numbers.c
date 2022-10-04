c43a9a78f391df944fd1d75bcef4bba12ef20840
prasimbaal <prasimbaal@gmail.com>
2022-10-04 05:48:58 -0700

    single digit numbers program

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void print_numbers(void)
+{
+  char i;
+  for(i=48;i<58;i++)
+    {
+      putchar(i);
+    }
+  putchar('\n');
+    }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+
+int _putchar(char c);
+void print_numbers(void);
 int mul(int a, int b);
 int _isupper(int c);
 int _isdigit(int c);
